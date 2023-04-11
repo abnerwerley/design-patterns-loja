f86b5ad2de898664dcd1852e13a3d7c52314a98e
Abner Werley <abnerwerley77@gmail.com>
2023-04-11 17:36:15 -0300

    :poop: poop: código exemplo para aplicar desconto extra

--- src/br/com/loja/orcamento/Orcamento.java
+++ src/br/com/loja/orcamento/Orcamento.java
@@ -6,10 +6,22 @@ public class Orcamento {
 
 	private BigDecimal valor;
 	private int quantidade;
+	private String situacao;
 
 	public Orcamento(BigDecimal valor, int quantidade) {
 		this.valor = valor;
 		this.quantidade = quantidade;
+	}
+
+	public void aplicarDescontoExtra() {
+		BigDecimal valorExtra = BigDecimal.ZERO;
+		if (situacao.equals("EM ANALISE")) {
+			valorExtra = new BigDecimal("0.05");
+		}
+		if (situacao.equals("APROVADO")) {
+			valorExtra = new BigDecimal("0.02");
+		}
+		this.valor = this.valor.subtract(valorExtra);
 	}
 
 	public BigDecimal getValor() {
