1f1a78974a55fe33c48193d231ea2aebe458c5b8
Abner Werley <abnerwerley77@gmail.com>
2023-04-11 14:34:15 -0300

    :recycle: refactor: strategy pattern para aumentar flexibilidade do calculo de impostos

--- src/TesteImpostos.java
+++ src/TesteImpostos.java
@@ -1,8 +1,9 @@
 import java.math.BigDecimal;
 
 import br.com.loja.imposto.CalculadoraDeImpostos;
+import br.com.loja.imposto.ICMS;
+import br.com.loja.imposto.ISS;
 import br.com.loja.orcamento.Orcamento;
-import br.com.loja.orcamento.TipoImposto;
 
 public class TesteImpostos {
 
@@ -10,13 +11,12 @@ public class TesteImpostos {
 		Orcamento orcamento = new Orcamento(new BigDecimal("1000"));
 		CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
 
-		BigDecimal impostoIcms = calculadora.calcular(orcamento, TipoImposto.ICMS);
-		BigDecimal impostoIss = calculadora.calcular(orcamento, TipoImposto.ISS);
+		BigDecimal impostoIcms = calculadora.calcular(orcamento, new ICMS());
+		BigDecimal impostoIss = calculadora.calcular(orcamento, new ISS());
 
 		System.out.println(impostoIcms.compareTo(new BigDecimal("100.0")) >= 0 ? "Sucesso, 100 reais de imposto ICMS"
 				: "Não deu certo.");
 		System.out.println(impostoIss.compareTo(new BigDecimal("60.0")) >= 0 ? "Sucesso, 60 reais de imposto ISS"
 				: "Não deu certo.");
 	}
-
 }
--- src/br/com/loja/orcamento/TipoImposto.java
+++
@@ -1,7 +0,0 @@
-package br.com.loja.orcamento;
-
-public enum TipoImposto {
-
-	ICMS,
-	ISS;
-}
