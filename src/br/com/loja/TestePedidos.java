9bfe73cf653fb78f718edb400a914f3b6623c97f
Abner Werley <abnerwerley77@gmail.com>
2023-04-12 10:21:20 -0300

    :recycle: refactor: pattern Command

--- src/br/com/loja/TestePedidos.java
+++ src/br/com/loja/TestePedidos.java
@@ -1,20 +1,18 @@
 package br.com.loja;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
-import br.com.loja.orcamento.Orcamento;
-import br.com.loja.pedido.Pedido;
+import br.com.loja.pedido.GeraPedido;
 
 public class TestePedidos {
 
     public static void main(String[] args) {
-        String cliente = "Ana da Silva";
-        Orcamento orcamento = new Orcamento(new BigDecimal("600"), 4);
-        LocalDateTime data = LocalDateTime.now();
+        String cliente = "Ana";
+        BigDecimal valorOrcamento = new BigDecimal("2000");
+        int quantidadeItens = 3;
 
-        Pedido pedido = new Pedido(cliente, data, orcamento);
-        System.out.println("Salva pedido no banco de dados.");
-        System.out.println("Enviar email com dados do novo pediod.");
+        GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
+        System.out.println(gerador.gerar());
+
     }
 }
---
+++ src/br/com/loja/pedido/GeraPedido.java
@@ -0,0 +1,41 @@
+package br.com.loja.pedido;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import br.com.loja.orcamento.Orcamento;
+
+public class GeraPedido {
+
+    private String cliente;
+    private BigDecimal valorOrcamento;
+    private int quantidadeItens;
+
+    public GeraPedido(String cliente, BigDecimal valorOrcamento, int quantidadeItens) {
+        this.cliente = cliente;
+        this.valorOrcamento = valorOrcamento;
+        this.quantidadeItens = quantidadeItens;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public int getQuantidadeItens() {
+        return quantidadeItens;
+    }
+
+    public BigDecimal getValorOrcamento() {
+        return valorOrcamento;
+    }
+
+    public Pedido gerar() {
+        Orcamento orcamento = new Orcamento(this.valorOrcamento, this.quantidadeItens);
+        LocalDateTime data = LocalDateTime.now();
+
+        System.out.println("Salva pedido no banco de dados.");
+        System.out.println("Enviar email com dados do novo pedido.");
+
+        return new Pedido(this.cliente, data, orcamento);
+    }
+}
